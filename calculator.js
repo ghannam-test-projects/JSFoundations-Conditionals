36df33c95579fb71f2e85170266438c9b3344e84
ghannam88 <mr.ghannam@gmail.com>
2021-04-19 03:13:19 +0300

    calculator has been created by g

--- calculator.js
+++ calculator.js
@@ -1,3 +1,26 @@
 // Define the numbers and the operation here
+let firstNumber = 9;
+let secondNumber = 9;
+let operation = "/";
 
-// Write if-statements here
+let result;
+
+if (operation == "+"){
+    result = firstNumber+secondNumber;
+    console.log(`the result of ${firstNumber} added to ${secondNumber} is ${result}`)
+
+}else if (operation == "-"){
+    result = firstNumber-secondNumber;
+    console.log(`the result of ${firstNumber} sustracted from ${secondNumber} is ${result}`)
+}else if (operation == "/"){
+    result = firstNumber/secondNumber;
+    console.log(`the result of ${firstNumber} devided by ${secondNumber} is ${result}`)
+}else if (operation == "*"){
+    result = firstNumber*secondNumber;
+    console.log(`the result of ${firstNumber} multiplied by ${secondNumber} is ${result}`)
+}
+else{
+    console.log("invalid result");
+}
+
+console.log(`Do you want to do more operations to the result ${result}?`);
